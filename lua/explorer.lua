0aadac9d331f7f19c5153bb65b6cd786f7150e55
Nicholas Koulopoulos <nickkoul13@gmail.com>
2024-01-15 21:44:39 -0700

    Refactor and Clean Up querying

--- lua/explorer.lua
+++ lua/explorer.lua
@@ -1,5 +1,9 @@
 local explorer = {}
 
+local function generateServerUrl(server)
+  return server.address .. ":" .. server.port
+end
+
 local function getServers()
  local servers = {}
 
@@ -12,19 +16,49 @@ local function getServers()
  return servers
 end
 
-function explorer.grpcExplore()
-  print(vim.inspect(getServers()))
-  local listCmd = "grpcurl -plaintext localhost:50051 list"
+local function getServices(server)
+  local listCmd = "grpcurl -plaintext " .. generateServerUrl(server) .. " list"
   local serviceListOutput = io.popen(listCmd)
-  if serviceListOutput ~= nil then
-    for serviceLine in serviceListOutput:lines() do
-      print("Service: " .. serviceLine)
-      local apiCmd = listCmd .. " " .. serviceLine
-      local apiOutput = io.popen(apiCmd)
-      if apiOutput ~= nil then
-        for apiLine in apiOutput:lines() do
-          print("\t" .. apiLine)
-        end
+
+  if serviceListOutput == nil then
+    return {}
+  end
+
+  local services = {}
+  for line in serviceListOutput:lines() do
+    table.insert(services, line)
+  end
+
+  return services
+end
+
+local function getApis(server, service)
+  local apiCmd = "grpcurl -plaintext " .. generateServerUrl(server) .. " list " .. service
+  local apiOutput = io.popen(apiCmd)
+
+  if apiOutput == nil then
+    return {}
+  end
+
+  local apis = {}
+  for line in apiOutput:lines() do
+    table.insert(apis, line)
+  end
+
+  return apis
+end
+
+
+function explorer.grpcExplore()
+  local servers = getServers()
+  for _, server in pairs(servers) do
+    print(server.name .. " - " .. generateServerUrl(server))
+    local services = getServices(server)
+    for _, service in pairs(services) do
+      print("\t Service: " .. service)
+      local apis = getApis(server, service)
+      for _, api in pairs(apis) do
+        print("\t\t" .. api)
       end
     end
   end
